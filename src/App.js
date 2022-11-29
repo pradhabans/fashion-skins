4fbba7823d29155342357dbf812e55a216329d85
pradhabans <pradhabanprabu@gmail.com>
2022-11-30 00:11:07 +0530

    implemented react-routes, Outlet, navigation bar and Links

--- src/App.js
+++ src/App.js
@@ -1,49 +1,23 @@
-import Directory from "./components/directory/directory.component";
+import Home from "./routes/home/home.component";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop = () => {
+  return <h1>I am Shop</h1>;
+};
+
+const Cart = () => {
+  return <h1> I am Cart</h1>;
+};
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "Womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "Mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
   return (
-    <Directory categories={categories} />
-    // <div className="categories-container">
-    //   {categories.map((category) => (
-    //     <CategoryItem key={category.id} category={category} />
-    //     // <div key={id} className="category-container">
-    //     //   <div
-    //     //     className="background-image"
-    //     //     style={{ backgroundImage: `url(${imageUrl})` }}
-    //     //   />
-    //     //   <div className="category-body-container">
-    //     //     <h2>{title}</h2>
-    //     //     <p>Shop Now</p>
-    //     //   </div>
-    //     // </div>
-    //   ))}
-    // </div>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="cart" element={<Cart />} />
+      </Route>
+    </Routes>
   );
 };
 
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,27 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import { ReactComponent as Crownlogo } from "../../assests/crown.svg";
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <Crownlogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/cart">
+            CART
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
